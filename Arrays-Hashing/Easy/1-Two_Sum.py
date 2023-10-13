f572bf6d35da8ab09c1cc0d6da0f13a8426f4d84
eerkmen <efeerkmen@gmail.com>
2023-10-13 19:27:32 +0000

    docs: add thought process and solution strategies to 1-Two_Sum.py

---
+++ Arrays-Hashing/Easy/1-Two_Sum.py
@@ -0,0 +1,15 @@
+#target - element should be equal to another element that exists in given array
+#exactly one solution
+
+#hashmap
+#map each value to the index
+#initialize hashmap empty
+#visited element is checked with the target - element equation. check if it exists in hashmap
+#everytime an element is visited add it to hashmap
+#gurantees that once the second element that adds up to the total value, first one is already in hashmap
+#time complexity: O(n)
+#memory complexity: O(n)
+
+#brute force
+#check every combination for every element
+#time complexity: O(n^2)
