2508aad723856d716fd3a9f8e789269296752e09
eerkmen <efeerkmen@gmail.com>
2023-10-15 22:57:00 +0000

    add code solution to leetcode 1929

--- Arrays-Hashing/Easy/1929-Concatenation_of_Array.py
+++ Arrays-Hashing/Easy/1929-Concatenation_of_Array.py
@@ -1,3 +1,10 @@
 #iterate through each value and append it to the end of array
 #time complexity: O(n)
 
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        ans = []
+        for i in range (2):
+            for n in nums:
+                ans.append(n)
+        return ans
