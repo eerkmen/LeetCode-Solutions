8de417be0817776f2f287fcc70326db343181e44
eerkmen <efeerkmen@gmail.com>
2023-10-15 21:21:08 +0000

    add comments to leetcode 27

---
+++ Arrays-Hashing/Easy/27-Remove_Element.py
@@ -0,0 +1,10 @@
+#in-place algorithm
+#take all of the values that are not special value placed at the beginning
+#relative order does not matter
+
+#two pointer method
+#left pointer is where we replace the values at
+#to increment left pointer
+#if the value the left pointer is at is not the special value, just swap it by itself and increment it by one
+#this way we don't have to think about edge cases calculating the result
+
