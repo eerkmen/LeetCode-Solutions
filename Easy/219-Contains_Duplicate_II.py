1823260ee454842633ece07d761054921cf73759
eerkmen <efeerkmen@gmail.com>
2023-10-16 20:03:39 +0000

    add comments to leetcode 219

---
+++ Easy/219-Contains_Duplicate_II.py
@@ -0,0 +1,21 @@
+#size of the window less or equal to k
+# k + 1 is equal to valid window
+#return boolean
+
+
+#brute force
+#check every single window
+
+
+
+#hashset/hashmap
+#look at values in O(1)
+#sliding window
+#if window is bigger than k
+# remove the value in index L
+# increment L by 1
+#check every value in index R
+#if true found duplicate
+#otherwise add the value
+
+
