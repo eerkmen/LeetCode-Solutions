478d4109c8442bb6108ee33c10d5c6aa49bf52fa
eerkmen <efeerkmen@gmail.com>
2023-10-23 00:00:00 +0000

    add comments to leetcode 49 file

---
+++ Medium/49-Group_Anagrams.py
@@ -0,0 +1,8 @@
+#sorting
+#two strings of each other if we take each of them and sort them
+#time complexity: O(m * nlogn) => m number of input strings, n is is for each string sorting
+
+#hashmap
+#a-z (26)
+#ex: eat count [a-z] = e:1, a:1, t:t
+#time complexity: O(m*n*26) => O(m*n)
