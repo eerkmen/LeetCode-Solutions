277647e2d5a2a1adde9aea48e832f1160ffd7b77
eerkmen <efeerkmen@gmail.com>
2023-10-27 13:51:10 -0400

    add comments to leetcode 682 file

--- Easy/682-Baseball_Game.py
+++ Easy/682-Baseball_Game.py
@@ -1,2 +1,9 @@
 #stack data structure
+#iterate through all of the operations
+#so if else case
+#if c pop
+#if d top of the stack double it and add to the stack
+#if + add previous score and add to the stack
+#return all of the values in stack summed
+#time complexity O(1)
 
