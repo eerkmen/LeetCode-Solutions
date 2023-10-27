b42c01bcc04adb17274f0d707f79a6ca0d124342
eerkmen <efeerkmen@gmail.com>
2023-10-27 14:01:15 -0400

    add coding solution to leetcode 682 file

--- Easy/682-Baseball_Game.py
+++ Easy/682-Baseball_Game.py
@@ -5,7 +5,8 @@
 #if d top of the stack double it and add to the stack
 #if + add previous score and add to the stack
 #return all of the values in stack summed
-#time complexity O(1)
+#time complexity: O(n) =>iterating every single input
+#space complexity: O(n)
 
 class Solution:
     def calPoints(self, operations: List[str]) -> int:
@@ -13,9 +14,12 @@ class Solution:
 
         for op in operations:
             if op == "+":
-
+              stack.append(stack[-1] + stack[-2])
             elif op == "D":
-
+              stack.append(2 * stack[-1])
             elif op == "C":
-
+              stack.pop
             else:
+               stack.append(int(op))
+
+            return sum(stack)
