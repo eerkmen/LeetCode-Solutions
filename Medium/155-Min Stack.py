c8202628767ba1a03aaf0134faefce0f01574da6
eerkmen <efeerkmen@gmail.com>
2023-10-29 00:00:00 -0400

    add return min element functionality to minStack function in leetcode 155 file

--- Medium/155-Min Stack.py
+++ Medium/155-Min Stack.py
@@ -32,7 +32,7 @@ class MinStack:
         return self.stack[-1]
 
     def getMin(self) -> int:
-        
+        return self.minStack[-1]
 
 
 # Your MinStack object will be instantiated and called as such:
