34112369c378e1b0c96497f3b1fd5428440ba80b
eerkmen <efeerkmen@gmail.com>
2023-10-29 00:00:00 -0400

    add minStack function to leetcode 155 file

--- Medium/155-Min Stack.py
+++ Medium/155-Min Stack.py
@@ -12,3 +12,28 @@
 #if -3 popped also pop from the min stack
 #time complexity: O(1)
 #space complexity: O(n) => actually O(2n) but constant is removed
+
+class MinStack:
+
+    def __init__(self):
+        
+
+    def push(self, val: int) -> None:
+        
+
+    def pop(self) -> None:
+        
+
+    def top(self) -> int:
+        
+
+    def getMin(self) -> int:
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
