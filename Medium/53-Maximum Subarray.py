ba8112c75f920c58842b79a7db4e6b9f56f6c4eb
eerkmen <efeerkmen@gmail.com>
2023-10-30 00:00:00 -0400

    add comments to leetcode 53

---
+++ Medium/53-Maximum Subarray.py
@@ -0,0 +1,25 @@
+#brute force
+#iterate through every single subarray
+#time complexity: O(n^3)
+#pseudocode:
+#starting value=i
+for(i=0...n-1):
+#j is ending index
+  for(j=i ...n-1):
+#k=computing sum of the array
+    for(k=i...j):
+      compute the sum
+
+#to save time
+#create currenSum add add the j
+#time complexity: O(n^2)
+#pseudocode:
+for(i=0...n-1)
+  for(j=i...n-1)
+    curSum + num[j]
+
+#to save more time
+#sliding window
+#ignore the negative values
+#if the sum is greater than 0 keep adding
+#else restart the sum from the next element
