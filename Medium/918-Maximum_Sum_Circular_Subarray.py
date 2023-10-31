3f6aa2aba9d724bb2f357674f0cbcf66c68e4322
eerkmen <efeerkmen@gmail.com>
2023-10-31 00:00:00 -0400

    add comment to leetcode 918

---
+++ Medium/918-Maximum_Sum_Circular_Subarray.py
@@ -0,0 +1,11 @@
+#circular= end connected to beginning
+#max subarray sum can have portion from end and the beginning
+
+#kadanes algorithm
+#be greedy as possible
+#keep tack of curMax, update globalMax using curMAx
+#globalMax must be initialized to any value, if initialized to 0  array with all negative values will give an error
+#compare curSum to the value we are at and update curMax using the max value
+
+#handling circular requirement
+#if global min contigious subarray sum is known, we can totalsum-globalMin
