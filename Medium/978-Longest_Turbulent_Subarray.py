a094948190f1e10bacf52019bdb3043c9d03337e
eerkmen <efeerkmen@gmail.com>
2023-11-02 00:00:00 -0400

    add comments to leetcode 978

---
+++ Medium/978-Longest_Turbulent_Subarray.py
@@ -0,0 +1,11 @@
+#brute force
+#check every single subarray
+#time complexity: O(n^2)
+
+#kadanes/sliding window
+#initialize left and right pointer
+#compare index R with R-1
+#if R and R-1 is equal: put left pointer to the current right pointers index, increment R by one
+#if turbulence: greater than and less than order is false: shift left pointers index to R-1's index
+#time complexity: O(n)
+#space complexity: O(1)
