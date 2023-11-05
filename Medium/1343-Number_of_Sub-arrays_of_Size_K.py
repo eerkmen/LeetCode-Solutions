174a3c85a4100aa4ddd0ec7ac8baa976f614eed2
eerkmen <efeerkmen@gmail.com>
2023-11-05 00:00:00 -0400

    add comments to leetcode 1343

---
+++ Medium/1343-Number_of_Sub-arrays_of_Size_K.py
@@ -0,0 +1,10 @@
+#sliding window
+#threshold =average greater or equal 
+#intialize Left, Right pointers
+#keep track of currentsum
+#if average lower than threshold
+#increment Right pointer by one
+#add value at right pointer to currentsum
+#if average is greater or equal to threshold
+#increment the left pointer by one
+#subtract the value from the current sum
