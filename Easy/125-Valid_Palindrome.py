b55de262589c0dbe57f1fc9fb2f818467e8f083c
eerkmen <efeerkmen@gmail.com>
2023-11-10 00:00:00 -0500

    add code to leetcode 125

--- Easy/125-Valid_Palindrome.py
+++ Easy/125-Valid_Palindrome.py
@@ -30,7 +30,18 @@ class Solution:
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        
+        L, R = 0, len(s) - 1
+
+        while L < R:
+            while L < R and not self.alphaNum(s[L]):
+                L += 1
+            while L < R and not self.alphaNum(s[R]):
+                R -= 1
+
+            if s[L].lower() != s[R].lower():
+                return False
+            L, R = L + 1, R - 1
+        return True
 
 
 
