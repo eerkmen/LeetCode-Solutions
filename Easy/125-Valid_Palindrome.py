292ebfb739f5704ec86b735c373a355850a44add
eerkmen <efeerkmen@gmail.com>
2023-11-10 00:00:00 -0500

    add comments to leetcode 125

---
+++ Easy/125-Valid_Palindrome.py
@@ -0,0 +1,10 @@
+#palindrome:reads the same as it does when it is reverse
+#ex: aba => aba
+#ignore non-alphanumeric characters
+#consider all strings as lowerCase
+#ignore the space
+
+#iterate through every single character
+#if the character.isalnum
+# include it.lower in the newstring
+#if true return true
