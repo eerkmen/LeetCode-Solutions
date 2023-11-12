25c30cffd34d42bcfa7011fb2478d238666b3226
eerkmen <efeerkmen@gmail.com>
2023-11-12 00:00:00 -0500

    add comment to leetcode 167

---
+++ Medium/167-Two_Sum_II.py
@@ -0,0 +1,13 @@
+#brute force
+#look every combination
+#time complexity: O(n^2)
+
+#two pointer method
+#initialize Left pointer at the beginning
+#initialize Right pointer at the end
+#if total sum is bigger than target
+# decrement right pointer by 1
+# if total sum is smaller than target
+# increment Left pointer by 1
+#return indeces
+#time complexity: O(n)
