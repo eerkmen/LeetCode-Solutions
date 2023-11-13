5d7bea38831f16e972c9673499a5b694642a52e8
eerkmen <efeerkmen@gmail.com>
2023-11-13 00:00:00 -0500

    add two pointer code to leetcode 11

--- Medium/11-Container_With_Most_Water.py
+++ Medium/11-Container_With_Most_Water.py
@@ -22,4 +22,16 @@ class Solution:
 
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        
+        res = 0
+        L, R = 0, len(height) - 1
+
+        while L < R:
+            area = (R - L) * min(height[L], height[R])
+            res = max (res, area)
+
+            if height[L] < height[R]:
+                L += 1
+            else:
+                R -= 1
+
+        return res
