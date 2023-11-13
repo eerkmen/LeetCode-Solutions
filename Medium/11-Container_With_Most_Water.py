1ddb3c03c5e787a292f51498550b3f46cdd0e8bc
eerkmen <efeerkmen@gmail.com>
2023-11-13 00:00:00 -0500

    add brute force code to leetcode 11

--- Medium/11-Container_With_Most_Water.py
+++ Medium/11-Container_With_Most_Water.py
@@ -4,4 +4,11 @@
 
 class Solution:
     def maxArea(self, height: List[int]) -> int:
+        res = 0
+
+        for L in range(len(height)):
+            for R in range(L + 1, len(height)):
+                area = (R - L) * min(height[L], height[R])
+                res = max (res, area)
         
+        return res
