b05b45040c8e7c0ee2751b3aad8fc1d11be2e46d
eerkmen <efeerkmen@gmail.com>
2023-11-15 00:00:00 -0500

    add high memory solution comment to leetcode 42

--- Hard/42-Trapping_Rain_Water.py
+++ Hard/42-Trapping_Rain_Water.py
@@ -2,3 +2,16 @@
 #take minimum of off left and right pointer
 #min(L,R) -h[i] => gives the area of water each column holds
 
+#solution 1
+# for every single position to know how much water we can trap at index,i
+# we need to know max left and right height of every single position
+#make array to store this calculation
+#scan through the array
+#calculate every single maxLeft position and maxRight position in different arrays
+# then take min of maxleft and maxright, allows us to know how much water we can trap
+#after the calculations do this: min(L,R) - h[i] > 0 
+#if equal to or less than zero no water is trapped
+#sum the values at min array
+#memory complexity: O(n)
+
+
