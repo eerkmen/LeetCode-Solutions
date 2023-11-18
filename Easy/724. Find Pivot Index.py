78b18da2b7d6c1615e83a841ca424aa5bfd2242f
eerkmen <efeerkmen@gmail.com>
2023-11-18 00:00:00 -0500

    add comments to leetcode 724

---
+++ Easy/724. Find Pivot Index.py
@@ -0,0 +1,9 @@
+#brute force
+#check and compare every subarray
+#time complexity: O(n^2)
+
+#prefix sums solution
+#calculate prefix array
+#for the right side subtract the left sum and pivot, this calculates right sum
+
+ 
