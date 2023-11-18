42054e16c2f1387b4e67087218e2da38c728048e
eerkmen <efeerkmen@gmail.com>
2023-11-18 00:00:00 -0500

    add code to leetcode 724

--- Easy/724. Find Pivot Index.py
+++ Easy/724. Find Pivot Index.py
@@ -8,4 +8,12 @@
  
  class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        
+        total = sum(nums)
+
+        leftSum = 0
+        for i in range(len(nums)):
+           rightSum = total - num[i] - leftSum
+           if leftSum == rightSum:
+              return i
+           leftSum += nums[i]
+        return -1
